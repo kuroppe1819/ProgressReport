05f93464f107e03ca4931634bbdaa1e40daa63fe
kuroppe <kuroppems@gmail.com>
2017-10-21 23:02:23 +0900

    subclassを作成した

---
+++ ProgressReport/DropView.swift
@@ -0,0 +1,19 @@
+//
+//  DropView.swift
+//  ProgressReport
+//
+//  Created by kuroppe on 2017/10/21.
+//  Copyright © 2017年 kuroppe. All rights reserved.
+//
+
+import Cocoa
+
+class DropView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
