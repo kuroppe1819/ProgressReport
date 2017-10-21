f1fc4d47e8d8558fb34329ce9ffc65744c548462
kuroppe <kuroppems@gmail.com>
2017-10-21 23:01:59 +0900

    新しいViewControllerを作成した

---
+++ ProgressReport/DropViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DropViewController.swift
+//  ProgressReport
+//
+//  Created by kuroppe on 2017/10/21.
+//  Copyright © 2017年 kuroppe. All rights reserved.
+//
+
+import Cocoa
+
+class DropViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
